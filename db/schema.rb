fbcdc1d599038d352d866b58fe330c24e2783115
slim1979 <chinezan@yandex.ru>
2017-08-24 17:32:14 +0300

    search migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823155215) do
+ActiveRecord::Schema.define(version: 20170823203802) do
 
   create_table "coupe_vans", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20170823155215) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.integer "start_station_id"
+    t.integer "finish_station_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
   create_table "seats_kinds", force: :cascade do |t|
