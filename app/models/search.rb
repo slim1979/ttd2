081cc2e5c8a94df5b219c69f1c2f958407ffcc91
slim1979 <chinezan@yandex.ru>
2017-08-24 17:33:53 +0300

    routes method for routes searches

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,14 @@
 class Search < ApplicationRecord
+  belongs_to :user
+
+  def routes
+    routes = []
+    Route.all.each do |route|
+      stations = route.railway_stations.map(&:id)
+      start_station = stations.include?(start_station_id)
+      finish_station = stations.include?(finish_station_id)
+      routes << route if start_station && finish_station
+    end
+    routes
+  end
 end
