863f0bf9b32bf3209b91c1c541e9265c18a97e8a
slim1979 <chinezan@yandex.ru>
2017-08-24 17:34:41 +0300

    mini crud

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,25 @@
 class TicketsController < ApplicationController
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render :new
+    end
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:user_id, :start_station_id, :finish_station_id, :train_id)
+  end
 end
