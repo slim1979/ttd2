56f9a75733c97fb9a6c4915e12633b99f3e49591
slim1979 <chinezan@yandex.ru>
2017-08-26 14:16:45 +0300

    belong to route association

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :train
+  belongs_to :route
   belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id
   belongs_to :finish_station, class_name: 'RailwayStation', foreign_key: :finish_station_id
 
@@ -12,13 +13,5 @@ class Ticket < ApplicationRecord
 
   def station(type)
     RailwayStation.find(send("#{type}_station_id"))
-  end
-
-  def train
-    Train.find(train_id)
-  end
-
-  def route
-    Route.find(route_id)
   end
 end
