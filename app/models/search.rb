8b50b3814edda9761b974af4908e58b1b5116307
slim1979 <chinezan@yandex.ru>
2017-08-26 16:19:18 +0300

    method routes improved

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,14 +1,11 @@
 class Search < ApplicationRecord
   belongs_to :user
+  belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id
+  belongs_to :finish_station, class_name: 'RailwayStation', foreign_key: :finish_station_id
 
   def routes
-    routes = []
-    Route.all.each do |route|
-      stations = route.railway_stations.map(&:id)
-      start_station = stations.include?(start_station_id)
-      finish_station = stations.include?(finish_station_id)
-      routes << route if start_station && finish_station
-    end
-    routes
+    start_station_routes = RailwayStationsRoute.where(railway_station: start_station)
+    finish_station_routes = RailwayStationsRoute.where(railway_station: finish_station)
+    start_station_routes.map(&:route) & finish_station_routes.map(&:route)
   end
 end
