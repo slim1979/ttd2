50b1a9fbe999be307c683d90300ee442a8d52d9d
slim1979 <chinezan@yandex.ru>
2017-08-28 00:44:09 +0300

    admin flag added to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826221205) do
+ActiveRecord::Schema.define(version: 20170827201335) do
 
   create_table "coupe_vans", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20170826221205) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
