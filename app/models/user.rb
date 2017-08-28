2ad69deca32d88f47743f2e67f152d9f345da88a
slim1979 <chinezan@yandex.ru>
2017-08-28 23:20:50 +0300

    has many tickets

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
+
+  has_many :tickets
 end
