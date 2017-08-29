65a7815d10e5782e5ce15f2bcfdf18ef0251eec8
slim1979 <chinezan@yandex.ru>
2017-08-29 22:36:57 +0300

    last name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827201335) do
+ActiveRecord::Schema.define(version: 20170829162051) do
 
   create_table "coupe_vans", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20170827201335) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "admin", default: false
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
