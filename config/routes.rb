7b00d0f924f0e7812b8913b44e2167304cbe7596
slim1979 <chinezan@yandex.ru>
2017-08-29 22:38:49 +0300

    routes changed for user registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   resources :tickets do
     get :filled_from_search, on: :member
   end
+
   resources :searches
 
   namespace :admin do
+    resources :users do
+      get :confirmed_users, on: :collection
+    end
     resources :trains do
       resources :vans, shallow: true
     end
-
     resources :railway_stations do
       patch :set_index, on: :member
       patch :set_shedule, on: :member
     end
-
     resources :tickets do
       get :filled_from_search, on: :member
     end
-
     resources :routes
   end
+
   get 'welcome/index'
 
   root 'welcome#index'
