254b381e42f9b0c05cc54596ead95503dc182b93
slim1979 <chinezan@yandex.ru>
2017-08-30 13:38:55 +0300

    changed actions from filled_bla_bla_ to new

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,6 +1,14 @@
 class Admin::TicketsController < Admin::BaseController
   before_action :set_ticket, only: %i[edit update destroy]
 
+  def new
+    @ticket = Ticket.new
+    @ticket.route_id = params[:route_id]
+    @ticket.start_station_id = params[:start_station_id]
+    @ticket.finish_station_id = params[:finish_station_id]
+    @ticket.train_id = params[:train_id]
+  end
+
   def create
     @ticket = Ticket.new(ticket_params)
 
@@ -12,14 +20,6 @@ class Admin::TicketsController < Admin::BaseController
   end
 
   def edit; end
-
-  def filled_from_search
-    @ticket = Ticket.new
-    @ticket.route_id = params[:route_id]
-    @ticket.start_station_id = params[:start_station_id]
-    @ticket.finish_station_id = params[:finish_station_id]
-    @ticket.train_id = params[:train_id]
-  end
 
   def update
     if @ticket.update(ticket_params)
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,15 +1,20 @@
 class TicketsController < ApplicationController
+  before_action :set_ticket, only: %i[show destroy]
+
   def new
     @ticket = Ticket.new
+    @ticket.route_id = params[:route_id]
+    @ticket.start_station_id = params[:start_station_id]
+    @ticket.finish_station_id = params[:finish_station_id]
+    @ticket.train_id = params[:train_id]
   end
 
   def index
-    @tickets = Ticket.where(user: current_user)
+    @tickets = current_user.admin? ? Ticket.all : Ticket.where(user: current_user)
   end
 
   def create
-    @ticket = Ticket.new(ticket_params)
-    @ticket.user = current_user
+    @ticket = current_user.tickets.new(ticket_params)
 
     if @ticket.save
       redirect_to @ticket
@@ -18,20 +23,9 @@ class TicketsController < ApplicationController
     end
   end
 
-  def show
-    @ticket = Ticket.find(params[:id])
-  end
-
-  def filled_from_search
-    @ticket = Ticket.new
-    @ticket.route_id = params[:route_id]
-    @ticket.start_station_id = params[:start_station_id]
-    @ticket.finish_station_id = params[:finish_station_id]
-    @ticket.train_id = params[:train_id]
-  end
+  def show; end
 
   def destroy
-    @ticket = Ticket.find(params[:id])
     @ticket.destroy
     redirect_to tickets_url, notice: 'Билет был успешно удален.'
   end
@@ -40,5 +34,9 @@ class TicketsController < ApplicationController
 
   def ticket_params
     params.require(:ticket).permit(:user_id, :start_station_id, :finish_station_id, :train_id, :route_id, :first_name, :last_name, :passport_serial, :passport_number)
+  end
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
   end
 end
