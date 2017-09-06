4fc0e6b73b41fd13760986fad13d1d3f9649910a
slim1979 <chinezan@yandex.ru>
2017-09-06 20:17:52 +0300

    beautify

--- app/models/van.rb
+++ app/models/van.rb
@@ -2,11 +2,11 @@ class Van < ApplicationRecord
   TYPE = { CoupeVan: 'Купейный', PlatzVan: 'Плацкартный',
            SleepingVan: 'Спальный', SedentaryVan: 'Сидячий' }.freeze
   VAN_SEATS_KIND = { 'SedentaryVan' => %i[seats],
-                'PlatzVan'      => %i[top_seats bottom_seats side_top_seats side_bottom_seats],
-                'CoupeVan'      => %i[top_seats bottom_seats],
-                'SleepingVan'   => %i[bottom_seats] }.freeze
-  PLACES =  { top_seats: 'Верхних', bottom_seats: 'Нижних', side_top_seats: 'Верхних боковых',
-              side_bottom_seats: 'Нижних боковых', seats: 'Сидячих' }
+                     'PlatzVan'      => %i[top_seats bottom_seats side_top_seats side_bottom_seats],
+                     'CoupeVan'      => %i[top_seats bottom_seats],
+                     'SleepingVan'   => %i[bottom_seats] }.freeze
+  PLACES = { top_seats: 'Верхних', bottom_seats: 'Нижних', side_top_seats: 'Верхних боковых',
+             side_bottom_seats: 'Нижних боковых', seats: 'Сидячих' }.freeze
 
   belongs_to :train
 
