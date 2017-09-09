5efdba43ce2129b117d1bf0728e6d21e39f1fafc
slim1979 <chinezan@yandex.ru>
2017-09-09 12:22:27 +0300

    able to search ticket even to unauthentificate users

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,4 @@
 class Search < ApplicationRecord
-  belongs_to :user
   belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id
   belongs_to :finish_station, class_name: 'RailwayStation', foreign_key: :finish_station_id
 
